846bbce4d66102970c725b7246027a50f36af3b9
Dominik <dominik.hollidt@googlemail.com>
2017-09-25 17:37:11 +0200

    funktioniert

--- server.c
+++ server.c
@@ -1,14 +1,33 @@
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <stdio.h>
 #include <sys/types.h>
-
+#include <unistd.h>
 int main(){
-    File * stream = fopen("Com",r);
 
-    if (stream){
-    
-    }    
+    int i =mkfifo("Com.fifo", 0400);
+            printf("i: %d", i);
+
+//Überprüfen ob i != 0
+/*    if (stream){
+        printf("i: %d", i);
+    }
     else{
         mkfifo("Com", 0400);
-        return EXIT_SUCESS;
+
+    }*/
+
+    char buf[10];
+    int bread;
+    while(1){
+        bread = ssize_t read(int Com,void *buf, size_t 10);
+        if (bread !=0 ){
+            write(1,buf,10);
+        }
     }
+
+
+
+
+    return EXIT_SUCCESS;
+}
