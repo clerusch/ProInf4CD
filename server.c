cd6d3e1aa584614c60ace84aa446a9468f9e6c92
MISTERX <dominik.hollidt@googlemail.com>
2017-09-26 21:52:00 +0200

    Verbesserung

--- server.c
+++ server.c
@@ -3,31 +3,34 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+
 int main(){
-
-    int i =mkfifo("Com.fifo", 0400);
-            printf("i: %d", i);
-
-//Überprüfen ob i != 0
-/*    if (stream){
-        printf("i: %d", i);
-    }
-    else{
-        mkfifo("Com", 0400);
-
-    }*/
-
     char buf[10];
-    int bread;
-    while(1){
-        bread = ssize_t read(int Com,void *buf, size_t 10);
+    int bread, fdes;
+    printf("1\n");
+    int i =mkfifo("Com.fifo", 0664);
+    if (i!=0){
+        printf("Error mkfifo\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("2\n");
+    fdes = open("Com.fifo", O_RDONLY | O_CREAT);
+    printf("2.5\n");
+    if (fdes == -1){
+        return EXIT_FAILURE;
+    }
+    printf("3");
+
+
+    while (1){
+        bread = read(fdes, buf,  10);
+        printf("4");
         if (bread !=0 ){
             write(1,buf,10);
+                printf("5");
         }
-    }
-
-
-
-
+}
     return EXIT_SUCCESS;
 }
